1165f635e2950cfb46e9ccbac3c2476481a820e8
Alex Sheluchin <alex@netquity.com>
2017-05-21 10:03:30 -0400

    ipython: set vi-mode and bind jj to escape

---
+++ .ipython/profile_default/startup/10-vi-bindings.py
@@ -0,0 +1,17 @@
+from IPython import get_ipython
+from prompt_toolkit.enums import DEFAULT_BUFFER
+from prompt_toolkit.filters import HasFocus, HasSelection, ViInsertMode, EmacsInsertMode
+from prompt_toolkit.key_binding.input_processor import KeyPress
+from prompt_toolkit.keys import Keys
+
+ip = get_ipython()
+insert_mode = ViInsertMode() | EmacsInsertMode()
+
+def escape_from_insert_mode(event):
+    " Map 'jj' to Escape. "
+    event.cli.input_processor.feed(KeyPress(Keys.Escape))
+
+# Register the shortcut if IPython is using prompt_toolkit
+if getattr(ip, 'pt_cli'):
+    registry = ip.pt_cli.application.key_bindings_registry
+    registry.add_binding('j', 'j', filter=ViInsertMode())(escape_from_insert_mode)
