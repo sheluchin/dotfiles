c21670ef09af0f3440fc7239634f25ef66de2c79
Alex Sheluchin <alex.sheluchin@gmail.com>
2021-06-23 09:19:59 -0400

    Keyboard shortcuts: use gdbus for raising windows
    This is needed since Ubuntu switched to Wayland by default. Just uses
    the javascript API for managing windows.

--- raise_terminal.sh
+++ raise_terminal.sh
@@ -1,6 +1,19 @@
-# https://askubuntu.com/a/200935
-# if ps aux | grep "[g]nome-terminal" > /dev/null
-if ps aux | grep "kitty" > /dev/null
- then xdotool windowactivate $(xdotool search --onlyvisible --class kitty)
- else kitty&
-fi
+gdbus call \
+  --session \
+  --dest org.gnome.Shell \
+  --object-path /org/gnome/Shell \
+  --method org.gnome.Shell.Eval "
+var mw =
+  global.get_window_actors()
+    .map(w=>w.meta_window)
+    .find(mw=>{
+        if (
+            mw.get_wm_class().includes('kitty') &&
+            // look for the kitty window that isn't the vimwiki window
+            !(mw.get_title().includes('vimwiki'))
+        ) {
+            return mw;
+        }
+        return false;
+    });
+mw && mw.activate(0)"
